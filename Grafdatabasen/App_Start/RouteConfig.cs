1e4e25c70c011707f421d73ef7c86cf75e1fba83
mattiasthiel <mattias@icreate.se>
2015-04-16 22:39:29 +0200

    Routeconfig och borttagna script

--- Grafdatabasen/App_Start/RouteConfig.cs
+++ Grafdatabasen/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Grafdatabasen
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Graf", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
