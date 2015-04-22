99aa261d92fd60f591a4360bf4dca73cdbaae9c3
mattiasthiel <mattias@icreate.se>
2015-04-22 10:56:29 +0200

    h

--- Grafdatabasen/ViewModels/SearchViewmodel.cs
+++ Grafdatabasen/ViewModels/SearchViewmodel.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Grafdatabasen.Models;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace Grafdatabasen.ViewModels
 {
     public class SearchViewModel
     {
+
+        private readonly List<string> lista1;
+
+        [Display(Name = " 1.")]
+        public int valdId { get; set; }
+
+        public IEnumerable<SelectListItem> Lista1
+        {
+            get { return new SelectList(lista1, "Id", "SearchItem1"); }
+        }
+
         public string SearchItem1 { get; set; }
         public string SearchItem2 { get; set; }
         public string SearchItem3 { get; set; }
@@ -15,5 +28,11 @@ namespace Grafdatabasen.ViewModels
         public string SearchItem5 { get; set; }
 
         public string Nod { get; set; }
+
+        //public SelectListItem SearchItem2 { get; set; }
+
+        //List<SelectListItem> lista2 = new List<SelectListItem>();
+        //lista2.Add(new SelectListItem { Text = "--Välj--", Value = "0" });
+
     }
 }
